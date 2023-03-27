c52326db5a651776443a6b308c8d9a367871b80a
rakuszz0 <jiwagila023@gmail.com>
2023-03-27 10:13:27 +0700

    Echo

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	// "net/http"
-	// "os"
+	"os"
 	"wayshub/database"
 	"wayshub/pkg/mysql"
 	"wayshub/routes"
@@ -29,8 +29,8 @@ func main() {
 
 	mysql.DatabaseInit()
 	database.RunMigration()
-	// var PORT = os.Getenv("PORT")
-	var PORT = "5000"
+	var PORT = os.Getenv("PORT")
+	// var PORT = "5000"
 	fmt.Println("server running :" + PORT)
 
 	routes.RouteInit(e.Group("/api/v1"))
